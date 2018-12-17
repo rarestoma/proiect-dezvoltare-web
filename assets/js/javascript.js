8247c9ffdb609bab40df9ce89f753c209fe265e6
Toma Rares <tomairares@gmail.com>
2018-12-17 21:56:19 +0200

    tests

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -105,3 +105,20 @@ function loadDoc() {
   xhttp.open("GET", "text.txt", true);
   xhttp.send();
 }
+
+function send() {
+  var http = new XMLHttpRequest();
+  var url = 'text.txt';
+  var params = 'orem=ipsum&name=binny';
+  http.open('POST', url, true);
+
+  //Send the proper header information along with the request
+  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+          alert(http.responseText);
+      }
+  }
+  http.send(params);
+}
