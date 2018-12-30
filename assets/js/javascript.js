403bb89d675865046c7ef207a0224a3fefa12117
Toma Rares <tomairares@gmail.com>
2018-12-30 16:53:36 +0200

    da

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -94,7 +94,21 @@ fetch('db.json')
         var info = new Array();
         info = data;
 
-        console.log(data);
+        function showData(){
+          var datatable = document.createElement('table');
+          datatable.setAttribute('id', 'data');
+
+          var tr = datatable.insertRow(-1);
+
+          for (var h = 0; h < info.length; h++) {
+              var th = document.createElement('th');
+              th.innerHTML = info[h];
+              tr.appendChild(th);
+          }
+
+          var div = document.getElementById('di');
+          div.appendChild(datatable);
+        }
 
       });
     }
@@ -102,21 +116,3 @@ fetch('db.json')
   .catch(function(err) {
     console.log('Fetch Error', err);
   });
-
-
-
-function showData(){
-  var datatable = document.createElement('table');
-  datatable.setAttribute('id', 'data');
-
-  var tr = datatable.insertRow(-1);
-
-  for (var h = 0; h < info.length; h++) {
-      var th = document.createElement('th');
-      th.innerHTML = info[h];
-      tr.appendChild(th);
-  }
-
-  var div = document.getElementById('di');
-  div.appendChild(datatable);
-}
