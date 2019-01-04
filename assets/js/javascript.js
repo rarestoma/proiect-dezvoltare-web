4070692521b241f12b1336e9567ec03f2e9f6ba9
Toma Rares <tomairares@gmail.com>
2019-01-04 18:03:19 +0200

    updates

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -38,7 +38,7 @@
                var button = document.createElement('input');
 
                button.setAttribute('type', 'button');
-               button.setAttribute('value', 'Remove');
+               button.setAttribute('value', 'Sterge');
 
                button.setAttribute('onclick', 'removeRow(this)');
 
@@ -81,71 +81,86 @@
 
 //AJAX
 
+
+//Get users
  var url  = "http://localhost:3000/contact";
-// var xhr  = new XMLHttpRequest()
-// xhr.open('GET', url, true)
-// xhr.onload = function () {
-// 	var datas = JSON.parse(xhr.responseText);
-//   console.log(datas);
-//   console.log(datas.contact[1].nume);
-// 	if (xhr.readyState == 4 && xhr.status == "200") {
-//
-//    console.log(datas);
-//    var info = new Array();
-//         info = datas;
-//         console.log(info);
-//
-//         var news = document.getElementsByClassName("news-story")[0];
-//
-//         for(var i = 0; i < info.contact.length; i++) {
-//             var h5 = document.createElement("h5");
-//             h5.innerHTML = info.contact[i].id;
-//             news.appendChild(h5);
-//             var p = document.createElement("p");
-//             p.innerHTML = info.contact[i].nume;
-//             news.appendChild(p);
-//
-//         }
-//
-// 	} else {
-// 		console.error(datas);
-// 	}
-// }
-// xhr.send(null);
+var xhr  = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.onload = function () {
+	var datas = JSON.parse(xhr.responseText);
 
-fetch(url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+	if (xhr.readyState == 4 && xhr.status == "200") {
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-
-        var info = new Array();
-        info = data;
+   var info = new Array();
+        info = datas;
 
         var news = document.getElementsByClassName("news-story")[0];
 
-        for(var i = 0; i < info.contact.length; i++) {
-            var h5 = document.createElement("h5");
-            h5.innerHTML = info.dogs[i].id;
+        for(var i = 0; i < info.length; i++) {
+            var h5 = document.createElement("input");
+            h5.setAttribute('value', info[i].id);
+            h5.setAttribute('id', "id");
+            var id = info[i].id;
             news.appendChild(h5);
-            var p = document.createElement("p");
-            p.innerHTML = info.dogs[i].name;
+
+            var p = document.createElement("input");
+            p.setAttribute('value', info[i].nume);
+            p.setAttribute('id', "nume");
             news.appendChild(p);
+
+            var p = document.createElement("input");
+            p.setAttribute('value', info[i].prenume);
+            p.setAttribute('id', "prenume");
+            news.appendChild(p);
+
+            var p = document.createElement("input");
+            p.setAttribute('value', info[i].email);
+            p.setAttribute('id', "email");
+            news.appendChild(p);
+
+            var p = document.createElement("input");
+            p.setAttribute('value', info[i].text);
+            p.setAttribute('id', "text");
+            news.appendChild(p);
+
+            var br = document.createElement("br");
+            news.appendChild(br);
+
+
+            var l = document.createElement("div");
+            l.setAttribute('class', 'line');
+
+            var button = document.createElement('input');
+
+            button.setAttribute('type', 'button');
+            button.setAttribute('value', 'Sterge');
+
+            button.setAttribute('onclick', 'deleteUser("id")');
+            button.setAttribute('onclick', 'deleteUser("id")');
+
+            news.appendChild(button);
+
+            var button = document.createElement('input');
+
+            button.setAttribute('type', 'button');
+            button.setAttribute('value', 'Upload');
+
+            button.setAttribute('onclick', 'uploadUser("id")');
+            button.setAttribute('onclick', 'uploadUser("id")');
+
+            news.appendChild(button);
+
+
+
+            news.appendChild(l);
 
         }
 
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error', err);
-  });
+	} else {
+		console.error(datas);
+	}
+}
+xhr.send(null);
 
 
 // Post a user
@@ -173,10 +188,12 @@ function inputData() {
 }
 
 // Delete a user
-function deleteUser(){
+function deleteUser(who){
 
   var xhr = new XMLHttpRequest();
-  xhr.open("DELETE", url+'/1', true);
+  var who = document.getElementById('id').value;
+  console.log(who);
+  xhr.open("DELETE", url+"/"+who, true);
   xhr.onload = function () {
   	var users = JSON.parse(xhr.responseText);
   	if (xhr.readyState == 4 && xhr.status == "200") {
@@ -186,4 +203,29 @@ function deleteUser(){
   	}
   }
   xhr.send(null);
+}
+
+// Update a user
+function uploadUser(){
+  var data = {};
+  data.nume  = document.getElementById("nume").value;
+  data.prenume  = document.getElementById("prenume").value;
+  data.email  = document.getElementById("email").value;
+  data.text = document.getElementById("text").value;
+  var json = JSON.stringify(data);
+
+  var xhr = new XMLHttpRequest();
+  var who = document.getElementById('id').value;
+
+  xhr.open("PUT", url+'/'+who, true);
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  xhr.onload = function () {
+  	var users = JSON.parse(xhr.responseText);
+  	if (xhr.readyState == 4 && xhr.status == "200") {
+  		console.table(users);
+  	} else {
+  		console.error(users);
+  	}
+  }
+  xhr.send(json);
 }
